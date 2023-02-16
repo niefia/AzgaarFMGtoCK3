95aa1273efa02f80c536c6c7c9631dc3b6d79662
niefia <99533148+niefia@users.noreply.github.com>
2023-02-16 20:19:03 +0000

    Updated GUI with image viewer

--- runprogram.py
+++ runprogram.py
@@ -1,8 +1,11 @@
 import tkinter as tk
+import customtkinter
 import shutil
 import subprocess
+import sys
 from tkinter import filedialog, Entry, Label
-
+import PIL
+from PIL import Image, ImageOps, ImageTk, ImageFilter
 
 scaling_factor = 0
 
@@ -10,8 +13,6 @@ def select_geojson_file():
     global file_path
     file_path = filedialog.askopenfilename(initialdir="/", title="Select Cells Geojson file",
                                            filetypes=(("geojson files", "*.geojson"), ("all files", "*.*")))
-    selected_directory_textbox.delete(1.0, tk.END)
-    selected_directory_textbox.insert(1.0, file_path)
 
     print(file_path)
 
@@ -38,6 +39,7 @@ def select_output_folder():
 
     print("scaling_factor:", scaling_factor)
 
+def run_generator():
     subprocess.run(["python", "colorrandom.py"], cwd=folder_path)
     subprocess.run(["python", "provinces.py", str(scaling_factor)], cwd=folder_path)
     subprocess.run(["python", "heightmap.py", str(scaling_factor)], cwd=folder_path)
@@ -52,6 +54,8 @@ def select_output_folder():
     subprocess.run(["python", "relGenChil.py"], cwd=folder_path)
     subprocess.run(["python", "religionGen.py"], cwd=folder_path)
     subprocess.run(["python", "riverGen.py", str(scaling_factor)], cwd=folder_path)
+    refresh_image()
+
 
 
 def store_scaling_factor(value):
@@ -63,36 +67,76 @@ def select_scaling_factor():
     scaling_factor = float(scaling_factor_entry.get())
     print("Scaling factor:", scaling_factor)
 
-root = tk.Tk()
+def add_image():
+    global image_file_path
+    image_file_path = filedialog.askopenfilename()
+    image = Image.open(image_file_path)
+    width, height = int(image.width / 8), int(image.height / 8)
+    image = image.resize((width, height))
+    canvas.config(width=image.width, height=image.height)
+    image = ImageTk.PhotoImage(image)
+    canvas.image = image
+    canvas.create_image(0, 0, image=image, anchor="nw")
+
+def refresh_image():
+    global image_file_path
+
+    # Re-open the image and resize it
+    image = Image.open(image_file_path)
+    width, height = int(image.width / 8), int(image.height / 8)
+    image = image.resize((width, height))
+
+    # Update the canvas size and image
+    canvas.config(width=image.width, height=image.height)
+    image = ImageTk.PhotoImage(image)
+    canvas.image = image
+    canvas.create_image(0, 0, image=image, anchor="nw")
+
+
+customtkinter.set_appearance_mode("dark")
+root = customtkinter.CTk()
 root.title("Azgaar to CK3")
+root.geometry("1200x512")
+frame = customtkinter.CTkFrame(master=root, width = 200, height =600)
+frame.pack(side="left", fill="y")
 
+canvas = tk.Canvas(root, width=750, height=600)
+canvas.pack()
 
+image_button = tk.Button(frame, text="Add Image", command=add_image, bg="white")
+image_button.pack(pady=15)
 
-scaling_factor_label = tk.Label(root, text="Enter scaling factor:")
+    # Create label for scaling factor
+scaling_factor_label = customtkinter.CTkLabel(frame, text="Enter scaling factor:")
 scaling_factor_label.pack()
 
-scaling_factor_entry = tk.Entry(root)
+# Create entry for scaling factor
+scaling_factor_entry = customtkinter.CTkEntry(frame)
 scaling_factor_entry.pack()
 
-scaling_factor_button = tk.Button(text="Enter scaling factor", command=select_scaling_factor)
-scaling_factor_button.pack()
+# Create button to select scaling factor
+scaling_factor_button = customtkinter.CTkButton(frame,text="Enter scaling factor", command=select_scaling_factor)
+scaling_factor_button.pack(padx=20, pady=10)
 
-select_geojson_file_button = tk.Button(text="Select Cells geojson file", command=select_geojson_file)
-select_geojson_file_button.pack()
-
-
-
-select_riversgeojson_file_button = tk.Button(text="Select Rivers geojson file", command=select_riversgeojson_file)
-select_riversgeojson_file_button.pack()
+select_geojson_file_button = customtkinter.CTkButton(frame, text="Select Cells geojson file", command=select_geojson_file)
+select_geojson_file_button.pack(padx=20, pady=10)
 
 
-select_json_file_button = tk.Button(text="Select JSON file", command=select_json_file)
-select_json_file_button.pack()
+select_riversgeojson_file_button = customtkinter.CTkButton(frame,text="Select Rivers geojson file", command=select_riversgeojson_file)
+select_riversgeojson_file_button.pack(padx=20, pady=10)
 
-select_output_folder_button = tk.Button(text="Select output folder and generate maps", command=select_output_folder)
-select_output_folder_button.pack()
 
-label = tk.Label(root, text="Output folder must be same folder as the .exe and .py files")
-label.pack()
+select_json_file_button = customtkinter.CTkButton(frame,text="Select JSON file", command=select_json_file)
+select_json_file_button.pack(padx=20, pady=10)
+
+select_output_folder_button = customtkinter.CTkButton(frame,text="Select output folder", command=select_output_folder)
+select_output_folder_button.pack(padx=20, pady=10)
+
+generate_button = customtkinter.CTkButton(frame,text="Generate maps", command=run_generator)
+generate_button.pack(padx=20, pady=10)
+
+label = customtkinter.CTkLabel(frame, text="Output folder must be same folder as the .exe and .py files")
+label.pack(padx=20, pady=10)
+
 
 root.mainloop()
