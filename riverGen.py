71fd553f3837ccb3d308674ab4616fe38e99bd7d
Mattias T <mntronslien@gmail.com>
2023-03-20 22:03:52 +0100

    VIP tributaries

--- riverGen.py
+++ riverGen.py
@@ -39,8 +39,75 @@ def paint_land_sea(heightmap_file, output_file):
     img.save(output_file, "PNG")
 
 
+# Function to check how many pixels of a colour surrounds a pixel
+def check_number_of_surrounding_color(img, pixel, color):
+    '''Checks the pixels around the given pixel for the given color. Returns the number of pixels found.'''
+    count = 0
+    # Check the pixel above
+    try:
+        if img.getpixel((pixel[0], pixel[1] - 1)) == color:
+            count += 1
+    except IndexError:
+        pass
+    # Check the pixel below
+    try:
+        if img.getpixel((pixel[0], pixel[1] + 1)) == color:
+            count += 1
+    except IndexError:
+        pass
+    # Check the pixel to the left
+    try:
+        if img.getpixel((pixel[0] - 1, pixel[1])) == color:
+            count += 1
+    except IndexError:
+        pass
+    # Check the pixel to the right
+    try:
+        if img.getpixel((pixel[0] + 1, pixel[1])) == color:
+            count += 1
+    except IndexError:
+        pass
+    return count
+
+
+def check_for_surrounding_color(img, pixel, color):
+    '''Checks the pixels around the given pixel for the given color. Returns the direction of the pixel if found, otherwise returns None.'''
+
+
+    # Check the pixel above
+    try:
+        if img.getpixel((pixel[0], pixel[1] - 1)) == color:
+            return 0, -1
+    except IndexError:
+        pass
+    # Check the pixel below
+    try:
+        if img.getpixel((pixel[0], pixel[1] + 1)) == color:
+            return 0, 1
+    except IndexError:
+        pass
+    # Check the pixel to the left
+    try:
+        if img.getpixel((pixel[0] - 1, pixel[1])) == color:
+            return -1, 0
+    except IndexError:
+        pass
+    # Check the pixel to the right
+    try:
+        if img.getpixel((pixel[0] + 1, pixel[1])) == color:
+            return 1, 0
+    except IndexError:
+        pass
+    return None
+
+
+
+
+
 def draw_rivers(landsea_image, rivers_geojson, output_file):
     """Implements the river-drawing algorithm, taking in a land-sea painted image and a river geojson file."""
+    # Set the pixel length that rivers extend into the sea
+    offshore_runoff_pixel_length = 3
 
     # load the painted land sea map
     img = Image.open(landsea_image)
@@ -51,11 +118,6 @@ def draw_rivers(landsea_image, rivers_geojson, output_file):
     # Create an ImageDraw object to draw on the image
     draw = ImageDraw.Draw(img)
 
-    # When drawing any river, monitor the number of magenta pixels that was detexted at river position.
-    # If the river ends on a magenta, remove all but a few (offshore_runoff_pixel_length) pixels from the river,
-    # to avoid offshore river overlaps.
-    finished_on_magenta = False
-    offshore_runoff_pixel_length = 3
 
     with open(rivers_geojson, "r") as riverData:
         data = json.load(riverData)
@@ -63,20 +125,27 @@ def draw_rivers(landsea_image, rivers_geojson, output_file):
     # get rivers
     rivers = data["features"]
 
+    # Order the list of rivers by parent ID, so that ID 0 is first, and ID 1 is second, etc.
+    # This is so that tributaries are rendered later than source rivers
+    rivers = sorted(rivers, key=lambda k: k["properties"]["parent"])
+
     print("Number of rivers: " + str(len(rivers)))
     rivers_count = 0
     for river in rivers:
         rivers_count += 1
-        # For debug so that we can easier undertand what is going on
+        # For debug so that we can easier understand what is going on
         # get river name
         river_name = river["properties"]["name"]
         #Get river type
         river_type = river["properties"]["type"]
         #Get river parent
         river_parent = river["properties"]["parent"]
-        # Print the river name, type and parent ID to the console.
-        print("Drawing river: " + river_name + " of type " + river_type + " with parent ID " + str(river_parent))
+        # Print the progress, river name, type and parent ID to the console.
+        print("=============================================")
+        print("River " + str(rivers_count) + " of " + str(len(rivers)) + ": " + river_name + " (" + river_type + ") " + "Parent ID: " + str(river_parent))
 
+
+        #Converting the GEOJSON coordinates to pixel coordinates
         flat_coords = np.array([item * scaling_factor for sublist in river["geometry"]["coordinates"]
                                 for item in sublist])
         centred_coords = flat_coords + np.tile([img_width / 2, img_height / 2], int(flat_coords.size / 2))
@@ -88,10 +157,6 @@ def draw_rivers(landsea_image, rivers_geojson, output_file):
             new_coords = np.floor(centred_coords[row]).reshape(1, 2) # This is the 2d Array that holds the coordinates of the river line.
             # Print the river coordinates to the console if debug is true
             try:
-                #NOTE: new_coords can be outside the image, so we need to check for that
-              #  print("River coordinates: " + str(new_coords) +
-              #    #      " Existing pixel colour: " + str(img.getpixel((new_coords[0][0], new_coords[0][1]))))
-
                 if (np.abs(new_coords - draw_coords[-1]).sum(axis=1) >= 2)[0]:  # if both x and y coordinates jump
                     jump_coord = np.argmin(np.abs(draw_coords[-1] - centred_coords[row]))  # the closest of x or y
                     new_coords[0, jump_coord] = draw_coords[-1, jump_coord]
@@ -111,7 +176,6 @@ def draw_rivers(landsea_image, rivers_geojson, output_file):
         #Check that the river is not empty
         if len(draw_coords) == 0:
             # Print out an error message and continue to the next river
-            print("=============================================")
             print("River is empty, skipping to next river")
             print("=============================================")
             continue
@@ -143,21 +207,77 @@ def draw_rivers(landsea_image, rivers_geojson, output_file):
         for pixel in draw_coords:
             if img.getpixel((pixel[0], pixel[1])) == blue:
                 # Print out an error message and continue to the next river
-                print("=============================================")
-                print("The river " + river_name + "  would draw over another river, skipping to next river")
-                print("=============================================")
+                print("The river " + river_name + " would draw over another river, trying to fix it.")
                 would_draw_over_another_river = True
                 break
         if would_draw_over_another_river:
-            #Print where in the for loop we are and what the total is
-            print("River " + str(rivers_count) + " of " + str(len(rivers)))
-            continue
+            # If we are about to overdraw another river,
+            # then we need to try to handle it by removing pixels from the river until we are valid.
+            # We will remove pixels from the end of the river until we are valid. Or until the river is empty.
+            while would_draw_over_another_river:
+                # Remove the last pixel from the river
+                # if this was the last pixel in the river, then we will abort this river
+                if len(draw_coords) < 1:
+                    print("Could not fix the river, skipping to next river")
+                    print("=============================================")
+                    break
+
+                #Remove the last pixel from the river
+                draw_coords = draw_coords[:-1]
+                # Check if the river is now valid
+                would_draw_over_another_river = False
+                for pixel in draw_coords:
+                    if img.getpixel((pixel[0], pixel[1])) == blue:
+                        would_draw_over_another_river = True
+                        #print("The river is still invalid, trying to fix it.")
+                        break
+        print("The river passed the direct overdraw check.")
+
+        # Time to correct the border
+        # If the rivers does not have valid borders,
+        # then we will start subtracting pixels from the end of the river
+        # until we have a valid river
+        has_invalid_border = False
+        for pixel in draw_coords:
+            if check_number_of_surrounding_color(img, pixel, blue) > 1:
+                has_invalid_border = True
+                print("The river " + river_name + " has an invalid border, trying to fix it.")
+                break
+        while has_invalid_border:
+            # Remove the last pixel from the river
+            # if this was the last pixel in the river, then we will abort this river
+            if len(draw_coords) < 1:
+                print("Could not fix the river border, skipping to next river")
+                print("=============================================")
+                break
+            #Remove the last pixel from the river
+            draw_coords = draw_coords[:-1]
+            # Check if the river is now valid
+            has_invalid_border = False
+            for pixel in draw_coords:
+                if check_number_of_surrounding_color(img, pixel, blue) > 1:
+                    has_invalid_border = True
+                    print("Still invalid borders, trying to fix it.")
+                    break
+        # #   # If the river is a tributary, we must now make sure that it is connected to the source river in a valid way
+        #    if river_parent != 0:
+        #        # If the river is a tributary, we must now make sure that it is connected to the source river in a valid way
+        #        if check_number_of_surrounding_color(img, draw_coords[0], green) < 1:
+        #            not_connected_to_source_river = True
+        #            print("This tributary is not connected to the source river, trying to fix it.")
+        #            break
+
+
 
 
         # start_color = green  # beginning color is green for source river
         # Draw the river to the map
         try:
             draw.line(sum(draw_coords.tolist(), []), fill=blue)  # draw the river line
+            # if the river is named "Fargo" then we will draw it in red
+            if river_name == "Fargo":
+                draw.line(sum(draw_coords.tolist(), []), fill=(255, 165, 0)) #Orange DEBUG DEBUG DEBUG
+
         except:
             print("Error: Pixel out of bounds")
         # If the river has 0 as a parent id, then it is a source river
@@ -177,6 +297,11 @@ def draw_rivers(landsea_image, rivers_geojson, output_file):
     img.save(output_file, "PNG")
     #System open the image for debug purposes
     img.show()
+
+
+# Define a method that checks a pixel and surrounding pixels for a specific color. Returns true or false.
+
+
 
 
 # Checks for Specific error where Multiple Blue pixels are around Green Source, replaces with White automatically
