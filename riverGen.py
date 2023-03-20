06129673178f5f5ec285556da6fc22144cd32f42
Mattias T <mntronslien@gmail.com>
2023-03-20 11:48:52 +0100

    Managed to crop a river runing into the ocean

--- riverGen.py
+++ riverGen.py
@@ -9,10 +9,16 @@ scaling_factor = 30
 # print the scaling factor
 print("Scaling factor: " + str(scaling_factor))
 
-#Constants for blue, greeen and magenta
-#blue = (0, 0, 255)
-#green = (0, 255, 0)
-#magenta = (255, 0, 255)
+# Constants for blue, greeen and magenta
+blue = (0, 0, 255)
+green = (0, 255, 0)
+magenta = (255,0,128)
+white = (255, 255, 255)
+red = (255, 0, 0)
+
+# Declare a debug variable
+debug = True
+
 
 def paint_land_sea(heightmap_file, output_file):
     """Takes in a heightmap file, paints land white and sea magenta, outputs a PIL image object."""
@@ -26,8 +32,8 @@ def paint_land_sea(heightmap_file, output_file):
 
     # Replace sea with magenta, land with white
     sea_pixels = ((red == 0) & (blue == 0) & (green == 0)).T
-    color_array[sea_pixels] = (255, 0, 128)  # replacing sea pixels
-    color_array[~sea_pixels] = (255, 255, 255)  # replacing "not-sea" pixels
+    color_array[sea_pixels] = magenta  # replacing sea pixels
+    color_array[~sea_pixels] = white  # replacing "not-sea" pixels
 
     img = Image.fromarray(color_array)
     img.save(output_file, "PNG")
@@ -45,85 +51,109 @@ def draw_rivers(landsea_image, rivers_geojson, output_file):
     # Create an ImageDraw object to draw on the image
     draw = ImageDraw.Draw(img)
 
-    with open(rivers_geojson, "r") as f:
-        data = json.load(f)
+    with open(rivers_geojson, "r") as riverData:
+        data = json.load(riverData)
 
     # get rivers
     rivers = data["features"]
 
+    print("Number of rivers: " + str(len(rivers)))
     for river in rivers:
-        flat_coords = np.array([item * scaling_factor for sublist in river["geometry"]["coordinates"]
-                                for item in sublist])
-        centred_coords = flat_coords + np.tile([img_width / 2, img_height / 2], int(flat_coords.size / 2))
-        centred_coords = centred_coords.reshape(int(len(centred_coords) / 2), 2)  # reshaping into 2-column array
-
-        if river["properties"]["parent"] == 0 or river["properties"]["parent"] == river["properties"]["id"]:
-            start_color = (0, 255, 0)  # beginning color is green for source river
-        else:
-            centred_coords = centred_coords[::-1]  # starting from the end of the river if the river ends in a parent
-            start_color = (255, 0, 0)  # beginning color is red for tributary river
-
-        draw_coords = np.floor(centred_coords[0]).reshape(1, 2)  # first coordinates to add
-
-        for row in range(1, centred_coords[1:].shape[0]):
-            new_coords = np.floor(centred_coords[row]).reshape(1, 2)
-            if (np.abs(new_coords - draw_coords[-1]).sum(axis=1) >= 2)[0]:  # if both x and y coordinates jump
-                jump_coord = np.argmin(np.abs(draw_coords[-1]-centred_coords[row]))  # the closest of x or y
-                new_coords[0, jump_coord] = draw_coords[-1, jump_coord]
-                draw_coords = np.append(draw_coords, new_coords, axis=0)
-            elif (np.abs(new_coords - draw_coords[-1]).sum(axis=1) == 1)[0]:  # at least one of x or y moves
-                draw_coords = np.append(draw_coords, new_coords, axis=0)
-
-        draw.line(sum(draw_coords.tolist(), []), fill=(0, 0, 255))
-        draw.point(draw_coords[0].tolist(), fill=start_color)  # just the starting point
-
-    img = img.transpose(Image.FLIP_TOP_BOTTOM)
-    img.save(output_file, "PNG")
+        # We will process all rivers
+        if river["properties"]:
 
 
-def draw_mainstreams(landsea_image, rivers_geojson, output_file):
-    """Implements the river-drawing algorithm, taking in a land-sea painted image and a river geojson file."""
+            # For debug sp that we can eisier undertand what is going on
+            # get river name
+            river_name = river["properties"]["name"]
+            #Get river type
+            river_type = river["properties"]["type"]
+            #Get river parent
+            river_parent = river["properties"]["parent"]
+            # Print the river name, type and parent ID to the console.
+            print("Drawing mainstream: " + river_name + " of type " + river_type + " with parent ID " + str(river_parent))
 
-    # load the painted land sea map
-    img = Image.open(landsea_image)
-    # Flip top to bottom for right coordinate alignment
-    img = img.transpose(Image.FLIP_TOP_BOTTOM)
-    # get image width and height
-    img_width, img_height = img.size
-    # Create an ImageDraw object to draw on the image
-    draw = ImageDraw.Draw(img)
-
-    with open(rivers_geojson, "r") as f:
-        data = json.load(f)
-
-    # get rivers
-    rivers = data["features"]
-
-    for river in rivers:
-        if river["properties"]["parent"] == 0: #If the river is a mainstrem, then the parent will be 0
             flat_coords = np.array([item * scaling_factor for sublist in river["geometry"]["coordinates"]
                                     for item in sublist])
             centred_coords = flat_coords + np.tile([img_width / 2, img_height / 2], int(flat_coords.size / 2))
             centred_coords = centred_coords.reshape(int(len(centred_coords) / 2), 2)  # reshaping into 2-column array
 
-            start_color = (0, 255, 0)  # beginning color is green for source river
-
             draw_coords = np.floor(centred_coords[0]).reshape(1, 2)  # first coordinates to add
 
+
+            # When drawing any river, monitor the number of magenta pixels that was detexted at river position.
+            # If the river ends on a magenta, remove all but a few (mageenta_pixel_limit) pixels from the river, to avoid offshore river overlaps.
+
+            magenta_pixel_count = 0
+            mageenta_pixel_limit = 3
+
+
+            # Find the pixel corrdinates of the river line
             for row in range(1, centred_coords[1:].shape[0]):
-                new_coords = np.floor(centred_coords[row]).reshape(1, 2)
+                new_coords = np.floor(centred_coords[row]).reshape(1, 2) #This is the 2d Array that holds the coordinates of the river line.
+                # Print the river coordinates to the console if debug is true, and it is the first river in the list.
+                if debug and river == rivers[0]:
+                    print("River coordinates: " + str(new_coords) + " Existing pixel colour: " + str(img.getpixel((new_coords[0][0], new_coords[0][1]))))
+                    #Also print the cuurrent image pixel colour to the console.
+
+
+
+                #Try if the pixel at the cooridnate is magenta, then we increment the magenta pixel count
+                try:
+                    if img.getpixel((new_coords[0][0], new_coords[0][1])) == magenta:
+                        magenta_pixel_count += 1
+                    #If the pixel is not magenta, then we reset the magenta pixel count
+                    else:
+                        magenta_pixel_count = 0
+                except:
+                    print("Error: Pixel out of bounds")
+
+                # TODO Better translation of coorinates to pixels
+                
                 if (np.abs(new_coords - draw_coords[-1]).sum(axis=1) >= 2)[0]:  # if both x and y coordinates jump
-                    jump_coord = np.argmin(np.abs(draw_coords[-1]-centred_coords[row]))  # the closest of x or y
+                    jump_coord = np.argmin(np.abs(draw_coords[-1] - centred_coords[row]))  # the closest of x or y
                     new_coords[0, jump_coord] = draw_coords[-1, jump_coord]
                     draw_coords = np.append(draw_coords, new_coords, axis=0)
                 elif (np.abs(new_coords - draw_coords[-1]).sum(axis=1) == 1)[0]:  # at least one of x or y moves
                     draw_coords = np.append(draw_coords, new_coords, axis=0)
 
-            draw.line(sum(draw_coords.tolist(), []), fill=(0, 0, 255))
-            draw.point(draw_coords[0].tolist(), fill=start_color)  # just the starting point
+                # If the magenta pixel limit is not 0. We will remove the last few pixels from the river line equal to the magenta pixel limit.
+                if mageenta_pixel_limit != 0:
+                    if magenta_pixel_count >= mageenta_pixel_limit:
+                        #Remove the last few pixels from the river line equal to the magenta pixel limit.
+                        draw_coords = draw_coords[:-mageenta_pixel_limit]
+                        #Set the magenta pixel count to 0
+                        magenta_pixel_count = 0
+
+            # We have the river coloured in, but not saved to the map, so now we need to do some error checking.
+            # First we need to check that this river isn't going to be drawn over another river.
+            # We do this by checking each pixel in the river line, and if it is already blue, we throw an error.
+
+            # For each pixel in the river line, check the pixel in the image.
+            #for pixel in draw_coords:
+                #Get the pixel colour value
+                #pixel_value = img.getpixel((pixel[0], pixel[1]))
+
+                # If the pixel is blue, then we have a problem.
+                #if img.getpixel((pixel[0], pixel[1])) == blue:
+                    #Throw an error and specify where the error is.
+                    #raise Exception("Error: River " + river_name + " is being drawn over another river at pixel " + str(pixel[0]) + ", " + str(pixel[1]) + ".")
+
+            # start_color = green  # beginning color is green for source river
+            # Draw the river river to the map
+            try:
+                draw.line(sum(draw_coords.tolist(), []), fill=blue)  # draw the river line
+            except:
+                print("Error: Pixel out of bounds")
+            # draw.point(draw_coords[0].tolist(), fill=start_color)  # Recolours the starting point of the river
+            # TODO: Error checking the area around the river to make sure all pixels are valid
+
 
     img = img.transpose(Image.FLIP_TOP_BOTTOM)
     img.save(output_file, "PNG")
+    #System open the image for debug purposes
+    img.show()
+
 
 # Checks for Specific error where Multiple Blue pixels are around Green Source, replaces with White automatically
 def correct_rivermap(image_path, output_file):
@@ -138,38 +168,38 @@ def correct_rivermap(image_path, output_file):
         for y in range(image.size[1]):
             for x in range(image.size[0]):
                 # Check if the pixel is green
-                if pixels[x, y] == (0, 255, 0):
+                if pixels[x, y] == green:
                     # Count the adjacent blue pixels
                     count = 0
                     adj_blues = []
-                    if x > 0 and pixels[x-1, y] == (0, 0, 255):
+                    if x > 0 and pixels[x - 1, y] == blue:
                         count += 1
-                        adj_blues.append((x-1, y))
-                    if x < image.size[0]-1 and pixels[x+1, y] == (0, 0, 255):
+                        adj_blues.append((x - 1, y))
+                    if x < image.size[0] - 1 and pixels[x + 1, y] == blue:
                         count += 1
-                        adj_blues.append((x+1, y))
-                    if y > 0 and pixels[x, y-1] == (0, 0, 255):
+                        adj_blues.append((x + 1, y))
+                    if y > 0 and pixels[x, y - 1] == blue:
                         count += 1
-                        adj_blues.append((x, y-1))
-                    if y < image.size[1]-1 and pixels[x, y+1] == (0, 0, 255):
+                        adj_blues.append((x, y - 1))
+                    if y < image.size[1] - 1 and pixels[x, y + 1] == blue:
                         count += 1
-                        adj_blues.append((x, y+1))
+                        adj_blues.append((x, y + 1))
                     # Check if the green pixel has more than one adjacent blue pixel
                     if count > 1:
                         for bx, by in adj_blues:
                             # Check if the blue pixel has only one adjacent blue pixel
                             bcount = 0
-                            if bx > 0 and pixels[bx-1, by] == (0, 0, 255):
+                            if bx > 0 and pixels[bx - 1, by] == blue:
                                 bcount += 1
-                            if bx < image.size[0]-1 and pixels[bx+1, by] == (0, 0, 255):
+                            if bx < image.size[0] - 1 and pixels[bx + 1, by] == blue:
                                 bcount += 1
-                            if by > 0 and pixels[bx, by-1] == (0, 0, 255):
+                            if by > 0 and pixels[bx, by - 1] == blue:
                                 bcount += 1
-                            if by < image.size[1]-1 and pixels[bx, by+1] == (0, 0, 255):
+                            if by < image.size[1] - 1 and pixels[bx, by + 1] == blue:
                                 bcount += 1
                             if bcount == 0:
                                 # Replace the blue pixel with white
-                                new_pixels[bx, by] = (255, 255, 255)
+                                new_pixels[bx, by] = white
                                 break
                         else:
                             print("Error at ({}, {})".format(x, y))
@@ -184,7 +214,8 @@ def correct_rivermap(image_path, output_file):
         # Save the new image
         new_image.save(output_file)
 
-#Use Landsea file as mask image as most Blue errors happen in sea, so can be masked anyway
+
+# Use Landsea file as mask image as most Blue errors happen in sea, so can be masked anyway
 def mask_landsea(image_path, mask_path, output_image_path):
     # Load the original image
     img = Image.open(image_path)
@@ -200,7 +231,7 @@ def mask_landsea(image_path, mask_path, output_image_path):
 
     # Iterate over all pixels in the mask image
     for x in range(width):
-        print (x,"/8192")
+        print(x, "/8192")
         for y in range(height):
             # Get the color of the pixel
             r, g, b = mask.getpixel((x, y))
@@ -211,7 +242,8 @@ def mask_landsea(image_path, mask_path, output_image_path):
     # Save the modified image
     img.save(output_image_path)
 
-#Checks for error of more than 2 touching Blue pixels, Does not correct yet
+
+# Checks for error of more than 2 touching Blue pixels, Does not correct yet
 def check_image_for_more_than_two_blue(image_path):
     # Load the image
     image = Image.open(image_path)
@@ -222,31 +254,31 @@ def check_image_for_more_than_two_blue(image_path):
     for y in range(image.size[1]):
         for x in range(image.size[0]):
             # Check if the pixel is green
-            if pixels[x, y] == (0, 0, 255):
+            if pixels[x, y] == blue:
                 # Count the adjacent blue pixels
                 count = 0
-                if x > 0 and pixels[x-1, y] == (0, 0, 255):
+                if x > 0 and pixels[x - 1, y] == blue:
                     count += 1
-                if x < image.size[0]-1 and pixels[x+1, y] == (0, 0, 255):
+                if x < image.size[0] - 1 and pixels[x + 1, y] == blue:
                     count += 1
-                if y > 0 and pixels[x, y-1] == (0, 0, 255):
+                if y > 0 and pixels[x, y - 1] == blue:
                     count += 1
-                if y < image.size[1]-1 and pixels[x, y+1] == (0, 0, 255):
+                if y < image.size[1] - 1 and pixels[x, y + 1] == blue:
                     count += 1
                 # Check if the green pixel has more than one adjacent blue pixel
                 if count > 2:
-                    print("Error Blue pixel with more than 2 adjacent at ({}, {})".format(x, y), "Blue Pixel Error:",(bpc))
+                    print("Error Blue pixel with more than 2 adjacent at ({}, {})".format(x, y), "Blue Pixel Error:",
+                          (bpc))
                     bpc += 1
-
 
 
 # Test Region of the code
 
-#Path to folder containing heightmap.png, landsea.png, rivers.geojson when testing
+# Path to folder containing heightmap.png, landsea.png, rivers.geojson when testing
 path = "River_Test/"
 
 paint_land_sea(path + "heightmap.png", path + "landsea.png")
-draw_mainstreams(path + "landsea.png", path + "rivers.geojson", path + "rivermap_mainstreams.png")
-correct_rivermap(path + "rivermap_mainstreams.png", path + "rivermap_corrected.png")
-mask_landsea(path + "rivermap_corrected.png", path + "landsea.png", path + "rivermap_masked.png")
-check_image_for_more_than_two_blue(path + "rivermap_masked.png")
+draw_rivers(path + "landsea.png", path + "rivers.geojson", path + "rivermap_mainstreams.png")
+#correct_rivermap(path + "rivermap_mainstreams.png", path + "rivermap_corrected.png")
+#mask_landsea(path + "rivermap_corrected.png", path + "landsea.png", path + "rivermap_masked.png")
+#check_image_for_more_than_two_blue(path + "rivermap_masked.png")
